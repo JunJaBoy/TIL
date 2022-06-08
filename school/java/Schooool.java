7097e408ce0d33fc07e26de9e3c309458dd3a752
Park Junsu <krpjs0508@gmail.com>
2022-06-08 12:09:56 +0900

    학교 20220608
    자바

---
+++ school/java/Schooool.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Schooool {
+    //20220608 안현수쌤
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int temp = 0;
+        if (a < b){
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        System.out.println(a - b);
+    }*/
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        if (a > 0) {
+            if (a % 2 == 0) {
+                System.out.println("짝수임다");
+            } else {
+                System.out.println("홀수임다");
+            }
+        }
+        else {
+            System.out.println("0임다");
+        }
+    }*/
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a > b){
+            if (a > c) {
+                System.out.println(a);
+            }
+            else {
+                System.out.println(c);
+            }
+        }
+        else {
+            if (b > c){
+                System.out.println(b);
+            }
+            else System.out.println(c);
+        }
+    }*/
+}
