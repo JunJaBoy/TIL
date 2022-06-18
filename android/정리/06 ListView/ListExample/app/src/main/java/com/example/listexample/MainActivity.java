70970a335f43f6b9121f073d7f1c8503bbe71305
Park Junsu <krpjs0508@gmail.com>
2022-06-18 23:08:59 +0900

    안드로이드 20220618
    ListView

---
+++ android/정리/06 ListView/ListExample/app/src/main/java/com/example/listexample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.listexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.list);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("박준수");
+        data.add("준수박");
+        data.add("수박준");
+        data.add("준박수");
+        data.add("수준박");
+        data.add("박수준");
+        adapter.notifyDataSetChanged();
+    }
+}
