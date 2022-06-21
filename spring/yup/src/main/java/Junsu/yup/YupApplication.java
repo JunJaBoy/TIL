b3abbe438cad339a81a69bed2b220f75a73689d6
Park Junsu <krpjs0508@gmail.com>
2022-06-21 21:50:39 +0900

    스프링 20220621
    프로젝트 생성

---
+++ spring/yup/src/main/java/Junsu/yup/YupApplication.java
@@ -0,0 +1,13 @@
+package Junsu.yup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YupApplication.class, args);
+	}
+
+}
---
+++ spring/yup/src/test/java/Junsu/yup/YupApplicationTests.java
@@ -0,0 +1,13 @@
+package Junsu.yup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
